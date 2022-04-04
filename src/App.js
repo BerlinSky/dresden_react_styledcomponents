95162393a653ea217e243847a13bd9967ac90818
Pedro Young <pedroyoung@livenation.com>
2022-04-04 12:44:55 -0400

    add utilities.js for styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';               
-import './App.css';                          
-                                             
-import Title from "./components/title/Title";
-                                             
-function App() {                             
-  return (                                   
-    <div className="App">                    
-      <header className="App-header">        
+import logo from './logo.svg';                
+import './App.css';                           
+                                              
+import Title from "./components/title/Title"; 
+import Button from "./components/button/Button";
+                                              
+function App() {                              
+  return (                                    
+    <div className="App">                     
+      <header className="App-header">         
         <img src={logo} className="App-logo" alt="logo" />
-        <Title />
-      </header>      
-      <title />      
-    </div>           
-  );                 
-}                    
-                     
-export default App;  
-                     
+        <Title />                             
+        <Button />                             
+      </header>                               
+      <title />                               
+    </div>                                    
+  );                                      
+}                                         
+                                          
+export default App;         
+                            
--- src/components/button/style.js
+++ src/components/button/style.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components';
-                          
-const StyledButton = styled.button`
- color: blue;             
-  font-size: 1em;         
-  margin: 1em;            
-  padding: 0.25em 1em;    
-  border: 2px solid blue; 
-  border-radius: 2rem;    
-  background: transparent;     
-`;                          
-                            
-export default StyledButton;
+import styled from 'styled-components';  
+import { colors } from "../../utilities";
+                                         
+const StyledButton = styled.button`      
+  color: ${colors.primary500};           
+  font-size: 1em;                        
+  margin: 1em;                           
+  padding: 0.25em 1em;                   
+  border: 2px solid ${colors.primary500};
+  border-radius: 2rem;                   
+  background: transparent;               
+`;                                       
+                                         
+export default StyledButton;             
